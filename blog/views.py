2c1f10d579710cddb55de8bc29719bbdad3cd090
Admin <HALLMA2K@outlook.com>
2021-03-23 01:02:16 -0700

    Major updates to website.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,85 @@
 from django.shortcuts import render
+from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
+from django.shortcuts import redirect
+from django.utils import timezone
+from django.core.mail import BadHeaderError, EmailMessage
+from django.http import HttpResponse
+from .forms import ContactForm
+
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def resume(request):
+    return render(request, 'resume.html', {})
+
+def home(request):
+    return render(request, 'home.html', {})
+
+def projects(request):
+    return render(request, 'projects/projects.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+def contact(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact_name = form.cleaned_data['contact_name']
+            contact_email = form.cleaned_data['contact_email']
+            content = form.cleaned_data['content']
+            try:
+                email = EmailMessage(contact_name,
+                                    content,
+                                    contact_email,
+                                    ['HALLMA2K@outlook.com'], #change to your email
+                                     reply_to=[contact_email],
+                                   )
+                email.send()
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('./thanks/')
+    return render(request, 'contact.html', {'form': form})
+
+
+def thanks(request):
+    return render(request, 'thanks.html', {})
+
 # Create your views here.
--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'EMAIL_PASSWORD'
+DEFAULT_FROM_EMAIL = 'webmaster@localhost'
